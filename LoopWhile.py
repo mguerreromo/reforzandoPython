142ba01f0ede4806855a53acac601f41f55ef52d
MGuerrero <103110110+mguerreromo@users.noreply.github.com>
2022-08-31 08:31:32 -0400

    Add files via upload

---
+++ LoopWhile.py
@@ -0,0 +1,14 @@
+def suma(num1, num2):
+    return(num1 + num2);
+
+def producto(num1, num2):
+    print(num1 / num2);
+num1 = int(input('Digita el numero 1: '));
+num2 = int(input('Digita le numero 2: '));
+
+
+if num1 > num2:
+    producto (num2, num1)
+else:
+    print('Libre de impuesto')
+
